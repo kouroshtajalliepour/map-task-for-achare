ff159065bd6672a895aee0d1c012f451c11c5e5a
kouroshtajalliepour <tajalliepour@protonmail.com>
2025-03-09 23:15:10 +0330

    map component added

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+// * imports
+import Map from "@/components/inputs/map.vue";
+
+const latLang = ref([35.6892, 51.389]);
+</script>
+
+<template>
+  <div>
+    <ClientOnly>
+      <Map class="form-map-input" v-model="latLang" />
+
+      <p>lat : {{ latLang[0] }}, lang : {{ latLang[1] }}</p>
+    </ClientOnly>
+  </div>
+</template>
+
+<style>
+.form-map-input {
+  width: 500px;
+  height: 500px;
+}
+</style>
---
+++ src/components/inputs/map.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+// * imports
+import { ref, onMounted, watch, defineProps, defineEmits } from "vue";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import Pointer from "@/components/icons/pointer.vue";
+
+// props and emits
+const props = defineProps<{
+  modelValue: number[];
+}>();
+const emit = defineEmits(["update:modelValue"]);
+
+// * data
+const mapContainer = ref(null);
+const center = ref(props.modelValue);
+
+// * watchers
+watch(
+  () => center.value,
+  () => {
+    emit("update:modelValue", center.value);
+  }
+);
+
+// * hooks
+onMounted(() => {
+  if (!mapContainer.value) return;
+
+  const map = L.map(mapContainer.value).setView(center.value, 13);
+
+  L.tileLayer("https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
+    attribution: "© Google Maps",
+  }).addTo(map);
+
+  map.on("moveend", () => {
+    const newCenter = map.getCenter();
+    center.value = [newCenter.lat, newCenter.lng];
+    console.log("Map Center:", center.value);
+  });
+});
+</script>
+
+<template>
+  <div class="map-input-wrapper">
+    <div class="skeleton-loader"></div>
+    <div ref="mapContainer" class="map-container"></div>
+    <Pointer class="pointer-icon" />
+  </div>
+</template>
+
+<style>
+.map-input-wrapper {
+  width: 100%;
+  height: 100%;
+  max-width: 800px;
+  max-height: 800px;
+  position: relative;
+  background-color: red;
+}
+.map-container {
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+.pointer-icon {
+  position: absolute;
+  width: 48px;
+  height: 48px;
+  z-index: 2;
+  top: calc(50% - 24px);
+  right: calc(50% - 24px);
+}
+
+.leaflet-popup-content {
+  direction: rtl;
+  text-align: right;
+  font-family: Vazir, Tahoma, sans-serif;
+}
+</style>
