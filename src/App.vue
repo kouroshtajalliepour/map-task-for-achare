5cb5fc532e97bc3ea576df4cc7c04133026f216a
Ali Arshadi <realgabral@protonmail.com>
2025-03-11 19:07:13 +0330

    navigation bar complete

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from '@/components/icons/logo.vue';
+</script>
 
 <template>
-  <nav>
-    <router-link to="/">اضافه کردن آدرس </router-link>
-    <router-link to="/addresses">آدرس ها</router-link>
+  <nav class="navigation-bar">
+    <Logo />
+    <div class="navigation-link-wrapper">
+      <router-link to="/">ثبت آدرس</router-link>
+      <router-link to="/addresses">مشاهده آدرس ها</router-link>
+    </div>
   </nav>
   <router-view />
 </template>
 
-<style></style>
+<style lang="scss">
+@import '@/assets/styles/global.scss';
+
+.navigation-bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #ffffff;
+  padding: 12px 16px;
+  margin-bottom: 24px;
+  a {
+    font-weight: 700;
+    margin-right: 20px;
+    font-size: 12px;
+    text-decoration: none;
+    color: $Primary-color;
+  }
+  .router-link-active {
+    color: $text-color;
+  }
+}
+</style>
--- src/stores/address.ts
+++ src/stores/address.ts
@@ -2,15 +2,15 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 type ICreateAddress = {
-  first_name: string
-  last_name: string
-  coordinate_mobile: string
-  coordinate_phone_number: string
-  address: string
-  lat: number,
-  lang: number,
-  gender: string
-}
+  first_name: string;
+  last_name: string;
+  coordinate_mobile: string;
+  coordinate_phone_number: string;
+  address: string;
+  lat: number;
+  lang: number;
+  gender: string;
+};
 
 export const useAddressesStore = defineStore('addresses', () => {
   const addresses = ref([]);
@@ -25,32 +25,31 @@ export const useAddressesStore = defineStore('addresses', () => {
     lang,
     gender,
   }: ICreateAddress) => {
-
     try {
-      const response = await fetch(
-        'https://stage.achareh.ir/api/karfarmas/address',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Basic MDk4MjIyMjIyMjI6U2FuYTEyMzQ1Njc4',
-          },
-          body: JSON.stringify({
-            first_name,
-            last_name,
-            coordinate_mobile,
-            coordinate_phone_number,
-            address,
-            lat,
-            lang,
-            gender,
-          }),
-        }
-      );
-      const result = await response.json();
-      console.log(result);
+      // const response = await fetch(
+      //   'https://stage.achareh.ir/api/karfarmas/address',
+      //   {
+      //     method: 'POST',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //       Authorization: 'Basic MDk4MjIyMjIyMjI6U2FuYTEyMzQ1Njc4',
+      //     },
+      //     body: JSON.stringify({
+      //       first_name,
+      //       last_name,
+      //       coordinate_mobile,
+      //       coordinate_phone_number,
+      //       address,
+      //       lat,
+      //       lang,
+      //       gender,
+      //     }),
+      //   }
+      // );
+      // const result = await response.json();
+      // console.log(result);
     } catch (error) {
-      throw error
+      throw error;
     }
   };
   const fetchAddresses = async () => {
@@ -69,7 +68,7 @@ export const useAddressesStore = defineStore('addresses', () => {
       addresses.value = data;
       console.log('Fetched addresses:', addresses.value);
     } catch (error) {
-      throw error
+      throw error;
       // * sanitize error response err.response._data.message
     }
   };
